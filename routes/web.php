42eff32cfad9e466c33f9577522449116273a76a
Dharmendra0007 <dharmendra@noesislearnings.com>
2023-09-18 13:45:32 +0530

    space

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('website-home');
 });
 Auth::routes();
+
+
 //Route::get('/login', 'LoginController@loginSso');
 Route::get('/login/s', 'LoginController@login');
 
