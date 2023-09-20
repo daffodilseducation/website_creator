25242653862c81cb56c2d8fd4d912363075a51cb
mohit <aryamohit.9272@gmail.com>
2023-09-20 13:41:20 +0530

    save menu with icon upload

--- app/Menu.php
+++ app/Menu.php
@@ -7,4 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 class Menu extends Model
 {
      protected $guarded =[];
+
+     public static function save_menus($req, $authUser){
+
+          $file = $req->file('icon');
+          if($file && $file->getClientOriginalName()){
+               $path = public_path() . '/menu/icons/'; 
+               if(!file_exists($path)) {
+                        mkdir($path, 0777, true);
+               }
+               $file->move($path, $file->getClientOriginalName());
+          }
+
+
+          $dt = array();
+          $dt['user_id'] = $authUser->id;
+          $dt['parent_id'] = $req->get('parent_id') ? $req->get('parent_id') : 0;
+          $dt['label'] = $req->get('label');
+          $dt['slug'] = $req->get('slug');
+          $dt['order'] = $req->get('order');
+          $dt['icon'] = $file ? $file->getClientOriginalName() : "";
+          $dt['status'] = 'Active';
+          $menus = new Menu($dt);
+          $menus->save();
+        
+    }
 }
